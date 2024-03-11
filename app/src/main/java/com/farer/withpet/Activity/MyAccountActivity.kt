59987bd934873dbdfbb2b1f3609f44f95302d006
poemfarer <poemfarer@gmail.com>
2024-03-11 17:47:49 +0900

    make Activities, xml

---
+++ app/src/main/java/com/farer/withpet/Activity/MyAccountActivity.kt
@@ -0,0 +1,15 @@
+package com.farer.withpet.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.farer.withpet.databinding.ActivityMyaccountBinding
+
+class MyAccountActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMyaccountBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/farer/withpet/Fragment/ListFragment.kt
+++ app/src/main/java/com/farer/withpet/Fragment/ListFragment.kt
@@ -1,15 +1,15 @@
-package com.farer.withpet
+package com.farer.withpet.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.farer.withpet.databinding.FragmentMapBinding
+import com.farer.withpet.databinding.FragmentListBinding
 
-class MapFragment : Fragment() {
+class ListFragment : Fragment() {
 
-    private val binding by lazy { FragmentMapBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentListBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
